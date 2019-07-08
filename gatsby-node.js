276f0f69a62a02384e2e0ea56b73cf7348da2575
Zoran Dasic <zorandsc1@gmail.com>
2019-07-08 15:25:35 +0200

    blogs

--- gatsby-node.js
+++ gatsby-node.js
@@ -40,4 +40,26 @@ exports.createPages = async function({ graphql, actions }) {
       },
     })
   })
+
+  //amaount of posts
+  const posts = data.posts.edges
+
+  //posts per page
+  const postsPerPage = 5
+
+  //numb of pages
+  const numPages = Math.ceil(posts.length / postsPerPage)
+
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blogs` : `/blogs/${i + 1}`,
+      component: path.resolve("./src/templates/blogListTemplate.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
 }
